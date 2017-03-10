9785ff27b9ac72b826ee07bef6c8fab61e6af749
Olav Morken <olav.morken@uninett.no>
2017-03-10 15:59:19 +0100

    common: Use `public_ip` host variable for generating /etc/hosts

--- terraform/testsetup.tf
+++ terraform/testsetup.tf
@@ -111,7 +111,7 @@ data "template_file" "masters_ansible" {
 }
 
 data "template_file" "workers_ansible" {
-    template = "$${name} ansible_host=$${ip} lb=$${lb_flag}"
+    template = "$${name} ansible_host=$${ip} public_ip=$${ip} lb=$${lb_flag}"
     count = "${var.worker_count}"
     vars {
         name  = "${element(openstack_compute_instance_v2.worker.*.name, count.index)}"
