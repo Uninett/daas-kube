5d7fb65cfd0c4ea83a165f57869262e8283540f6
Gurvinder Singh <gurvinder.singh@uninett.no>
2017-03-29 20:45:18 +0200

    updated mysql deployment script to be able to deploy based on same appname but from different namesapces. also updated some basic resources and settings as discussed

--- admin-scripts/delete_mysqldb.sh
+++ admin-scripts/delete_mysqldb.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
-# set -e
-# set -o pipefail
+set -e
+set -o pipefail
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 application-name"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 namespace application-name"
 	exit 1
 fi
 
-APPNAME=$1
+NAMESPACE=$1
+APPNAME=$2
 
-echo "Delete Mysql Database for $APPNAME"
+echo "Delete Mysql Database for $NAMESPACE-$APPNAME"
 
-POD=$(kubectl -n mysql get po -l app=$APPNAME -o=jsonpath='{.items[*].metadata.name}')
-#kubectl -n mysql exec -ti $POD /usr/local/bin/delete_db.sh $APPNAME
-kubectl -n mysql delete secret $APPNAME
-kubectl -n mysql delete service $APPNAME
-kubectl -n mysql delete configmap $APPNAME
-kubectl -n mysql delete deployment $APPNAME
-kubectl -n mysql delete networkpolicy $APPNAME
-
+POD=$(kubectl -n mysql get po -l app=$NAMESPACE-$APPNAME -o=jsonpath='{.items[*].metadata.name}')
+kubectl -n mysql exec -ti $POD /usr/local/bin/delete_db.sh $APPNAME
+kubectl -n mysql delete secret $NAMESPACE-$APPNAME
+kubectl -n mysql delete service $NAMESPACE-$APPNAME
+kubectl -n mysql delete configmap $NAMESPACE-$APPNAME
+kubectl -n mysql delete deployment $NAMESPACE-$APPNAME
+kubectl -n mysql delete networkpolicy $NAMESPACE-$APPNAME
